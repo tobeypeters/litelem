aca60bf4af7f7dc38e0a878504ec5b4c5abefbec
tobeypeters <35942395+tobeypeters@users.noreply.github.com>
2023-11-28 20:04:59 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# Lit Element Generator
+
+[![Visual Studio Marketplace Version](https://img.shields.io/visual-studio-marketplace/v/your-username.your-extension-name)](https://marketplace.visualstudio.com/items?itemName=your-username.your-extension-name)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Effortlessly generate empty Lit elements in Visual Studio Code.
+
+## Features
+
+- Quickly scaffold Lit elements for efficient development.
+- Dynamically names the class and custom element based on the file name.
+
+## Installation
+
+1. Open Visual Studio Code.
+2. Go to the Extensions view (`Ctrl+Shift+X`).
+3. Search for "Lit Element Generator".
+4. Click Install.
+
+## Usage
+
+1. Open a file where you want to generate a Lit element.
+2. Use the command palette (`Ctrl+Shift+P`) and search for "Generate Lit Element".
+3. The extension will insert an empty Lit element template into the current file.
+
+## Configuration
+
+No additional configuration is required.
+
+## Contributing
+
+If you encounter any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+## License
+
+This extension is licensed under the [MIT License](LICENSE).
+
+---
+
+**Enjoy Lit development with ease! 🚀**
---
+++ extension.js
@@ -0,0 +1,77 @@
+// 	The MIT License(MIT)
+// Copyright(c), Tobey Peters, https://github.com/tobeypeters
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Description:
+// 	Template:  Code for a Lit element
+
+const vscode = require('vscode');
+
+function activate(context) {
+  // Register the command to generate Lit element
+  let disposable = vscode.commands.registerCommand('extension.generateLitElement', () => {
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+      // Get the current file name without extension
+      const fileNameWithoutExtension = vscode.window.activeTextEditor.document.fileName.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, '');
+
+      // Generated Lit element code with dynamic class name
+      const litElementCode = `
+        import { LitElement, html, css } from 'lit';
+
+        class ${capitalizeFirstLetter(fileNameWithoutExtension)} extends LitElement {
+          static styles = css\`
+            /* Add your styles here */
+          \`;
+
+          render() {
+            return html\`
+              <!-- Your content here -->
+            \`;
+          }
+        }
+
+        customElements.define('${toKebabCase(fileNameWithoutExtension)}', ${capitalizeFirstLetter(fileNameWithoutExtension)});
+      `;
+
+      // Insert the code into the editor
+      editor.edit(editBuilder => {
+        const currentPosition = editor.selection.active;
+        editBuilder.insert(currentPosition, litElementCode);
+      });
+    }
+  });
+
+  // Register the command for disposal
+  context.subscriptions.push(disposable);
+}
+
+// Deactivate function (optional)
+function deactivate() {}
+
+// Helper function to capitalize the first letter
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Helper function to convert string to kebab case
+function toKebabCase(str) {
+  return str.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
+}
+
+module.exports = {
+  activate,
+  deactivate
+};
